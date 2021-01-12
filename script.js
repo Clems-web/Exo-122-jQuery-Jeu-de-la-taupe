c45a1bf4fd03e4e7cc4edd5facc40daa3d4b13d7
Clems-web <clement.servais1@gmail.com>
2021-01-12 11:59:11 +0100

    Pouce

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $(".hole");       // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
+const scoreBoard = $(".score"); //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
+const moles = $(".mole");       // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+
+$("#startGame").click(function StartGame() {
+    let time = 10;
+    
+})
